e17a5b4534ea7922768c3da3e3b6ea29ae5f00c8
yphcodeBace <995095730@qq.com>
2020-07-06 11:23:57 +0800

    router

--- dva-quickstart/src/routes/IndexPage.js
+++ dva-quickstart/src/routes/IndexPage.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+const arr = [1,2,3,4,5,6];
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+class IndexPage extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      yph:'yanpeihao'
+    }
+  }
+  goTo(){
+    console.log(this.state.yph);
+    this.props.history.push('/Products');
+  }
+  renderArr(){
+    arr.forEach((element,index) => {
+      let a = index;
+      console.log(a);
+    });
+  }
+  componentDidMount(){
+    this.renderArr()
+  }
+  render(){
+    return (
+      <div className={styles.normal}>
+        <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+        <div className={styles.welcome} />
+        <ul className={styles.list}>
+          <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+          <li onClick={this.goTo.bind(this)}>Getting Started</li>
+          {/* <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li> */}
+        </ul>
+      </div>
+    );
+  }
 }
-
-IndexPage.propTypes = {
-};
-
 export default connect()(IndexPage);
